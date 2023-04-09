db306b6b4579e3c82cb4a9eac19ba5c73fee1efa
Hellen678 <119007444+Hellen678@users.noreply.github.com>
2023-04-09 16:32:07 +0300

    Update Field.cpp

--- Field.cpp
+++ Field.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class Field{};
+class Field{
+ const int fld_widght1 = 4
+ const int fls_height1 = 4
+ const int fld_widght2 = 5
+ const int fls_height2 = 5
+ const int fld_widght3 = 6
+ const int fls_height3 = 6
+   
+   
+};
+
+  
 // size of field -- options to choise
